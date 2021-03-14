f2458a2aa00ac4907e3a11a7ec677ebfe4bbd3da
Anujyad31 <anuj.311997@gmail.com>
2021-03-15 01:51:43 +0530

    Add project files.

---
+++ WebTest/Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTest.Context;
+
+namespace WebTest.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+
+        db_testEntities dbObj = new db_testEntities();
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddStudent(tbl_student model)
+        {
+            tbl_student obj = new tbl_student();
+            obj.Name = model.Name;
+            obj.Email = model.Email;
+
+            dbObj.tbl_student.Add(obj);
+            dbObj.SaveChanges();
+
+
+            return View("Student");
+
+        }
+
+    }
+}
